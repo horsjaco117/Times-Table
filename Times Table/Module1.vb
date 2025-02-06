c1b9270114707cf67eb11dbdae174062a7ac3b84
horsjaco117 <jacob_horsley@live.com>
2025-02-05 17:33:25 -0700

    Add project files.

---
+++ Times Table/Module1.vb
@@ -0,0 +1,41 @@
+﻿'Jacob Horsley
+'2/5/2025
+'RCET 2206
+'URL:
+
+Option Explicit On
+Option Strict On
+
+
+
+Module Module1
+    Sub Main()
+
+
+        Dim userInput As String
+        Dim justDoIt As Integer
+        Dim result As String
+        userInput = Console.ReadLine()
+
+        justDoIt = CInt(userInput)
+        Console.WriteLine($"You entered {userInput}!")
+
+
+
+
+        For i = 1 To justDoIt
+
+            For j = 1 To justDoIt
+                result = (i * j).ToString
+                'result = CStr(i * j)
+                'result = (i * j).ToString()
+                result = result.PadLeft(4)
+                'result = StrReverse(result)
+                Console.Write(result)
+            Next
+            Console.WriteLine()
+        Next
+
+    End Sub
+
+End Module
---
+++ Times Table/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Times Table")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Times Table")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8bce4271-3d6d-41ea-b287-ab461fdbd2b8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
